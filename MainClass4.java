9f8b5ca6d718fdb7d0f6febc338f1f66c39def14
Tejas Kandregula <97377142+Tejas-kandregula@users.noreply.github.com>
2024-10-22 22:26:34 +0530

    Update MainClass4.java

--- MainClass4.java
+++ MainClass4.java
@@ -1,5 +1,5 @@
 package interface2;
-//
+// MULTIPLE INHERITANCE WITH INTERFACE
 interface Sample1{
 	void test1();
 }
@@ -13,19 +13,19 @@ class Demo implements Sample3{
 
 	@Override
 	public void test1() {
-		// TODO Auto-generated method stub
+		
 		System.out.println(" test1 done");
 	}
 
 	@Override
 	public void test2() {
-		// TODO Auto-generated method stub
+		
 		System.out.println("test2 done");
 	}
 
 	@Override
 	public void test3() {
-		// TODO Auto-generated method stub
+		
 		System.out.println("test3 done");
 		
 	}
@@ -34,7 +34,7 @@ class Demo implements Sample3{
 public class MainClass4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Demo d1 = new Demo();
 		d1.test1();
 		d1.test2();
