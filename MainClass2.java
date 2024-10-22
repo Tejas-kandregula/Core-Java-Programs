eebe5b1ca77aa42c1faf9c959ec2f2e6cbda8763
Tejas Kandregula <97377142+Tejas-kandregula@users.noreply.github.com>
2024-10-22 22:23:13 +0530

    Update MainClass2.java

--- MainClass2.java
+++ MainClass2.java
@@ -1,5 +1,5 @@
 package interface1;
-
+// MULTI LEVEL INHERITANCE WITH INTERFACE
 interface Sample1{
 	void test1();
 }
@@ -13,19 +13,19 @@ class Demo1 implements Sample3{
 
 	@Override
 	public void test1() {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Test1 done");
 	}
 
 	@Override
 	public void test2() {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Test2 done");
 	}
 
 	@Override
 	public void test3() {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Test3 done");
 	}
 	
@@ -33,7 +33,7 @@ class Demo1 implements Sample3{
 public class MainClass2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Demo1 d1 = new Demo1();
 		d1.test1();
 		d1.test2();
